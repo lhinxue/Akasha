8f27dddee2c6496e0577c1d48ada5171bb0f6222
lhinxue <lhinxue@gmail.com>
2023-01-16 22:33:58 +1300

    Dev

---
+++ src/app/Akasha.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Editor from "./Component/Editor/Editor";
+
+function App() {
+  return (
+    <div className="App">
+      <Editor/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Editor from "./app/components/Editor/Editor";
+
+function App() {
+  return (
+    <div className="App">
+      <Editor/>
+    </div>
+  );
+}
+
+export default App;
